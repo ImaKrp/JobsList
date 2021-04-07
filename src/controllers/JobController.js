fc560c7b920355a2350e5f0aacd12e93f9155010
Júlio Gonçalves <julio.c04@alunos.ifsc.edu.br>
2021-04-06 21:45:38 -0300

    sqlite and changes

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -6,7 +6,7 @@ module.exports = {
     save(req, res){
         const jobs = Job.get()
         const lastId = jobs[jobs.length - 1]?.id || 0;
-        jobs.push({
+        Job.create({
             id: lastId + 1,
             name:req.body.name,
             "daily-hours":req.body["daily-hours"],
